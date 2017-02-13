9a8fb7a1b666960b3ef94af58380bd305b3e382d
borghol <khaled.abulborghol@gmail.com>
2017-02-13 14:35:52 +0400

    memberA First Commit

---
+++ src/lab3/LAB3.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab3;
+
+/**
+ *
+ * @author b00059898
+ */
+public class LAB3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello ya 6g3a");
+    }
+    
+}
