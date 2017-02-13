1516b1340d348b0dc459c66417b162eb49f5642b
borghol <khaled.abulborghol@gmail.com>
2017-02-13 14:47:57 +0400

    BUG FIXED

--- src/lab3/LAB3.java
+++ src/lab3/LAB3.java
@@ -16,7 +16,7 @@ public class LAB3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello ya 6g3a");
+        System.out.println("Hello World");
     }
     
 }
